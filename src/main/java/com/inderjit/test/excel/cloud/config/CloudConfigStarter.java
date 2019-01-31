953a569c33a440b829adedf1a8ddd377d31a386e
Inderjit <inderjitsanhotra@gmail.com>
2019-01-31 07:31:56 +0530

    Initial Commit

---
+++ src/main/java/com/inderjit/test/excel/cloud/config/CloudConfigStarter.java
@@ -0,0 +1,25 @@
+package com.inderjit.test.excel.cloud.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *
+ * @author Inderjit Singh Sanhotra
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigStarter {
+
+    static Logger logger = LoggerFactory.getLogger(CloudConfigStarter.class);
+
+    public static void main(String... args) {
+        logger.info("***************************Starting cloud configuration server ****************");
+        SpringApplication.run(CloudConfigStarter.class, args);
+    }
+}
